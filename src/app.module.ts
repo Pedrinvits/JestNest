1d4fc27eea8bd9501a14023d51b21b6dad333b85
Pedro Mantovani <pedromantovani26@gmail.com>
2024-07-30 15:27:25 -0300

    feat : add Posts module to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
+import { PostModule } from './posts/posts.module';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [UsuarioModule,PostModule],
 })
 export class AppModule {}
