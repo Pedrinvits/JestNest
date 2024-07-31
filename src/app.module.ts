57e9e070ce329111cd4228f3ccccc58cf17594b1
Pedro Mantovani <pedromantovani26@gmail.com>
2024-07-31 10:06:52 -0300

    feat : add postgres service config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
 import { PostModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsuarioModule,PostModule],
+  imports: 
+  [
+    UsuarioModule,
+    PostModule,
+    ConfigModule.forRoot({
+      isGlobal : true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass : PostgresConfigService,
+      inject : [PostgresConfigService]
+    })
+  ],
 })
 export class AppModule {}
